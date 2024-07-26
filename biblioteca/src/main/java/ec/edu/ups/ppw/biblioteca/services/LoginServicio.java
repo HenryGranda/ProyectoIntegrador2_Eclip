58f24b2a24e6f30b135735748767ff7f3eb498af
ezhigue <erickzhigue@gmail.com>
2024-07-26 15:21:57 -0500

    Arreglo usuarios

--- biblioteca/src/main/java/ec/edu/ups/ppw/biblioteca/services/LoginServicio.java
+++ biblioteca/src/main/java/ec/edu/ups/ppw/biblioteca/services/LoginServicio.java
@@ -1,26 +1,17 @@
 package ec.edu.ups.ppw.biblioteca.services;
 
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import ec.edu.ups.ppw.biblioteca.business.GestionUsuarios;
-import ec.edu.ups.ppw.biblioteca.dao.UsuarioDAO;
 import ec.edu.ups.ppw.biblioteca.model.Usuario;
 import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
-import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.inject.Inject;
-import jakarta.json.Json;
-import jakarta.json.JsonObject;
 
 
 @Path("/auth")
@@ -43,6 +34,11 @@ public class LoginServicio  {
         }
     }
 
-
+	@GET
+	@Produces("application/json")
+	public List<Usuario> list(){
+		List<Usuario> usuarios = gUsuarios.getUsuarios();
+		return usuarios;
+	}
 
 }
